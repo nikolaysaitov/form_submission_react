5051ba98563a0f92d3929c5bb2f56dace4f4643d
Nikolay Saitov <saitovkms@gmail.com>
2022-10-04 14:57:23 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+
+import emailjs from "emailjs-com";
+import { Form, Input, Button, TextArea } from 'semantic-ui-react';
+import Swal from 'sweetalert2';
 import logo from '../src/images/logo.svg';
 import './App.css';
 
 
-function App() {
+
+function App () {
+  const SERVICE_ID = "service_xjj6kda";
+  const TEMPLATE_ID = "template_au8lwme";
+  const USER_ID = "aZuwMPnueVkC4IMoq";
+    const handleOnSubmit = (e) => {
+      e.preventDefault();
+      emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
+        .then((result) => {
+          console.log(result.text);
+          Swal.fire({
+            icon: "success",
+            title: "Сообщение отправлено"
+          })
+        }, (error) => {
+          console.log(error.text);
+          Swal.fire({
+            icon: "error",
+            title: "Упсс, что-то пошле не так...",
+            text: error.text,
+          })
+        });
+      e.target.reset()
+    };
 
 
 
-
-
-  function handleSubmit(e) {
-    e.preventDefault();
-  }
   return (
-    <div className="container form__container">
+    <div className="App container form__container">
     <div className="form__hello">
       <img className="form__logo" src={logo} alt="Логотип" />
       <h1 className="form__header">Отправка формы!</h1>
-      <form onSubmit={handleSubmit} className="form__register validation={formCallbackValidation}">
+      <Form onSubmit={handleOnSubmit} className="form__register validation={formCallbackValidation}">
         <p className="form__text">Имя</p>
         <input
           name="name"
+          control={Input}
           placeholder="Имя"
           className="form__input"
           required
@@ -33,6 +56,7 @@ function App() {
         <p className="form__text">Email</p>
         <input
           type="email"
+          control={Input}
           name="email"
           placeholder="Email"
           className="form__input"
@@ -44,31 +68,27 @@ function App() {
           // onFocus={onFocus}
         />
         {/* {errors.email && <ValidText type="auth">{errors.email}</ValidText>} */}
-        <p className="form__text">Пароль</p>
-        <input
-          type="password"
-          name="password"
-          placeholder="Пароль"
+        <p className="form__text">Сообщение</p>
+        <textarea
+          type="text"
+          control={TextArea}
+          name="message"
+          placeholder="Текст"
           className="form__input"
           required
-          id="password-input"
-          // onInput={handleChange2}
-          // isValid={!errors.password}
-          // value={values.password  || ''}
-          // onFocus={onFocus}
-        />
+        ></textarea>
         {/* {errors.password && (
           <ValidText type="auth">{errors.password}</ValidText>
         )} */}
         <span className=""></span>
-        <button
+        <Button
           type="submit"
           className="form__submit"
           
         >
           Отправить
-        </button>
-      </form>
+        </Button>
+      </Form>
     </div>
   </div>
   );
