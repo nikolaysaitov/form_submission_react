628d9608df56df22c85a03c11870ea66ebe04ad2
Nikolay Saitov <saitovkms@gmail.com>
2022-10-04 13:36:49 +0300

    верстка

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
+import logo from '../src/images/logo.svg';
 import './App.css';
 
+
 function App() {
+
+
+
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container form__container">
+    <div className="form__hello">
+      <img className="form__logo" src={logo} alt="Логотип" />
+      <h1 className="form__header">Отправка формы!</h1>
+      <form onSubmit={handleSubmit} className="form__register validation={formCallbackValidation}">
+        <p className="form__text">Имя</p>
+        <input
+          name="name"
+          placeholder="Имя"
+          className="form__input"
+          required
+          id="name-input"
+          // onInput={handleChange2}
+          // isValid={!errors.name}
+          // value={values.name || ''}
+          // onFocus={onFocus}
+        />
+        {/* {errors.name && <ValidText type="auth">{errors.name}</ValidText>} */}
+        <p className="form__text">Email</p>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          className="form__input"
+          required
+          id="email-input"
+          // onInput={handleChange2}
+          // isValid={!errors.email}
+          // value={values.email || ''}
+          // onFocus={onFocus}
+        />
+        {/* {errors.email && <ValidText type="auth">{errors.email}</ValidText>} */}
+        <p className="form__text">Пароль</p>
+        <input
+          type="password"
+          name="password"
+          placeholder="Пароль"
+          className="form__input"
+          required
+          id="password-input"
+          // onInput={handleChange2}
+          // isValid={!errors.password}
+          // value={values.password  || ''}
+          // onFocus={onFocus}
+        />
+        {/* {errors.password && (
+          <ValidText type="auth">{errors.password}</ValidText>
+        )} */}
+        <span className=""></span>
+        <button
+          type="submit"
+          className="form__submit"
+          
         >
-          Learn React
-        </a>
-      </header>
+          Отправить
+        </button>
+      </form>
     </div>
+  </div>
   );
 }
 
